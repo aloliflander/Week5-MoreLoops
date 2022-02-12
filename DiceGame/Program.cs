3eb814959eaacf28b976b3325682ad944bbcf66e
alo liflander <alo.liflander@gmail.com>
2022-02-12 19:57:55 +0200

    Loops

---
+++ DiceGame/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace DiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userScore = 0;
+            int cpuSCore = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+              
+                Random rnd = new Random();
+            int cpuRandom = rnd.Next(1, 7);
+            int userRandom = rnd.Next(1, 7);
+            Console.WriteLine($"Arvuti viskas {cpuRandom}. Kasutaja viskas {userRandom}");
+          if(cpuRandom < userRandom)
+                {
+                    Console.WriteLine();
+                    userScore = userScore +1;
+                }
+          else if(cpuRandom > userRandom)
+                {
+                    Console.WriteLine();
+                    cpuSCore = cpuSCore + 1;
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine($"Arvuti skoor on {cpuSCore}, kasutaja skoor on {userScore}");
+            if(cpuSCore < userScore)
+            {
+                Console.WriteLine("Kasutaja on mängu võitnud. Palju õnne!");
+            }
+            else if(cpuSCore > userScore)
+            {
+                Console.WriteLine("Arvuti on mängu võitnud");
+            }
+            else
+            {
+                Console.WriteLine("Mäng on viigis");
+            }
+
+        }
+    }
+}
---
+++ LoginValidation/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LoginValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada kasutajatunnus ja salasõna
+            //kui sisestatud kasutajatunnus on "admin" ja salasõna on "admin1234"
+            //siis konsoolis kuvatakse "Tere tulemast"
+            //muul juhul konsoolis kuvatakse "Vale kasutajatunnus või salasõna. Proovi uuesti."
+            //Kasutajal on kolm katset.
+            int i = 0;
+            while (i < 3)
+            {
+            Console.WriteLine("Sisesta kasutajatunnus.");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Siseta salasõna.");
+            string userPassword = Console.ReadLine();
+            if(userName == "admin" && userPassword == "admin1234")
+            {
+                Console.WriteLine("Tere tulemast.");
+                    break;
+            }
+            else
+            {
+                    i = i + 1;
+                Console.WriteLine("Vale kasutajatunnus võu salasõna. Proovi uuesti");
+                Console.WriteLine($"{3-i} katset on jäänud");
+            }
+            }
+        }
+    }
+}
